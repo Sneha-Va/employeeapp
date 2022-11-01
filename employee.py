274ffdc9d0404d0388785e2299b2811db60c7a24
Snrha-Va <sneha.varghese@nestgroup.net>
2022-11-01 19:07:13 +0530

    first command

---
+++ employee.py
@@ -0,0 +1,21 @@
+while True:
+    print("select an option from the menu")
+    print("1 add employee")
+    print ("2 view all employee")
+    print("3 search a employee")
+    print("4 update the employee")
+    print("5 delete the employee")
+    print("6 exit")
+    choice=int(input('enter the option:'))
+    if (choice==1):
+        print("employee enter selectrd")
+    elif (choice==2):
+        print("view all employee")
+    elif (choice==3):
+      print("searchall employee")
+    elif (choice==4):
+         print("update all employee")
+    elif (choice==5):
+        print("delete all employee")
+    elif (choice==6):  
+        break
